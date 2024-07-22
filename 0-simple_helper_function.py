323bb2b6c3eccc3dba2ff01accbb89695dd8dd1f
baragu <baraguantonyy@gmail.com>
2024-07-23 00:00:00 +0300

    update task: simple helper function

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+"""
+simpler helper function
+"""
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters
+    """
+    return ((page - 1) * page_size, page * page_size)
